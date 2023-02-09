e7cf7b10dcad1b09c4ac49668e4f95e15d9b47e4
bilalaymak <bilalaymak@gmail.com>
2023-02-09 17:32:37 +0300

    updated

---
+++ src/main/java/practices/inheritance/mathematics/Circle.java
@@ -0,0 +1,7 @@
+package practices.inheritance.mathematics;
+
+public class Circle extends Mathematics{
+
+    double numberOfPi = 3.14;
+
+}
---
+++ src/main/java/practices/inheritance/mathematics/Runner.java
@@ -0,0 +1,16 @@
+package practices.inheritance.mathematics;
+
+public class Runner {
+
+    //Create a method that calculates the area of the circle (use inheritance for math operations)
+
+    public static void main(String[] args) {
+        areaOfCircle(7);
+    }
+
+    public static void areaOfCircle(int radius) {
+        Circle circle = new Circle();
+        System.out.println(circle.multiply(circle.numberOfPi, circle.findSquare(radius)));
+    }
+
+}
